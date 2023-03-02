8a430a03b38a91b8fa8b0f96b8e63d1f7538711e
Tran Khanh Duy <duytrk.uet@gmail.com>
2023-03-02 22:39:22 +0700

    creat

---
+++ Beginning.py
@@ -0,0 +1,33 @@
+# Eg 1: Mang trong numpy///////////////
+
+# baseball = [180, 225, 192, 189, 203, 210]
+
+# import numpy as np
+
+# np_baseball = np.array(baseball)
+
+# print(np_baseball)
+
+
+# Eg 2: Tuong tac voi list/////////////
+
+# x = ["a", "b", "c", "d"]
+# x[1] = "r"
+# x[2:] = ["s", "t"]
+# print(x)
+
+
+# Eg 3: Numpy///////////////////////
+
+# height = [74, 74, 72, 72, 73, 69, 69, 71, 76, 71, 73, 73, 74]
+
+
+# import numpy as np
+
+# np_height = np.array(height)
+
+# print(np_height)
+
+# np_height_m = np.array(np_height)*0.0254
+
+# print(np_height_m)
